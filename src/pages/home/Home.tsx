360b60218dbfe9ae8ad242a5081c82a6f7135dd3
oddodd-o <eehd10@naver.com>
2025-01-18 22:28:48 +0900

    checkbox

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
 import { Button } from "../../components/ui/button";
+import {Card} from "../../components/ui/card.tsx";
+import {Checkbox} from "../../components/ui/checkbox.tsx";
 
 export default function Home() {
   return (
     <div className="py-8">
       <h1 className="text-3xl font-bold">Home Page</h1>
       <Button>Click Me</Button>
+        <Card title="Card Title">dddd</Card>
+        <Checkbox title={"Checkbox"} />
     </div>
   );
 }
